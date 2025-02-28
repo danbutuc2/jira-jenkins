b61b579f092eaf3251a9922770ba1edb9963af44
Dan Butuc <146419517+danbutuc2@users.noreply.github.com>
2025-02-28 14:57:43 +0100

    Update create_jira_issue.py

--- create_jira_issue.py
+++ create_jira_issue.py
@@ -3,8 +3,8 @@ from jira import JIRA  # type: ignore
 
 # Jira server URL and credentials from environment variables
 jira_server = 'https://danbutuc.atlassian.net/'
-jira_user = os.getenv('JIRA_USER')
-jira_password = os.getenv('JIRA_PASSWORD')
+jira_user = 'danbutuc@gmail.com'
+jira_password = 'ATATT3xFfGF0bN-niXGKB107fwFq9aKrtLFNeSeZw3Yez1Yvc3PSqI6nJqiAhDiR5MC1pgHjs0FF11dLKJiHAbHnDsoTrD_k7XvRHhnS3HQPG19kTAteO_wbFy833aHKdXYSR56GbBN850GC3puG4G6zqRMQduyOIbabbhUHT32R_ZDA1lQCXNs=E8336908'
 
 # Connect to Jira
 jira = JIRA(server=jira_server, basic_auth=(jira_user, jira_password))
